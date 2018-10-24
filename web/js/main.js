2901009c56ced0362bf324e790e200d162c97e25
rafaelpancione <rafaelpancione@hotmail.com>
2018-10-24 16:56:52 -0300

    js
    animações banner

--- web/js/main.js
+++ web/js/main.js
@@ -10,3 +10,18 @@ $(".rslides-portifolio").responsiveSlides({
   timeout: 5000,          // Integer: Time between slide transitions, in milliseconds
   pager: true,           // Boolean: Show pager, true or false
 });
+
+Visibility.onVisible(function(){
+  setTimeout(function () {
+    $(".intro h1").addClass("animated fadeInDown");
+  }, 400);
+    setTimeout(function () {
+    $(".intro blockquote").addClass("animated fadeInDown");
+  }, 600);
+        setTimeout(function () {
+    $(".intro .btn").addClass("animated fadeInDown");
+  }, 800);
+    setTimeout(function () {
+    $(".intro-geral h1").addClass("animated fadeInDown");
+  }, 200);
+});
