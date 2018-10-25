352f56fb01386a24a503e08772c21474dd0f7698
rafaelpancione <rafaelpancione@hotmail.com>
2018-10-25 10:07:50 -0300

    form-envio
    Formulario de envio incompleto, falta o php para poder enviar.

--- web/js/main.js
+++ web/js/main.js
@@ -25,3 +25,56 @@ Visibility.onVisible(function(){
     $(".intro-geral h1").addClass("animated fadeInDown");
   }, 200);
 });
+//formulario
+$('.formphp').on('submit', function() {
+  var emailContato = ""; //Escreva aqui seu e-mail
+
+  var that = $(this),
+      url = that.attr('action'),
+      type = that.attr('method'),
+      data = {};
+
+  that.find('[name]').each(function(index, value) {
+    var that = $(this),
+        name = that.attr('name'),
+        value = that.val();
+
+    data[name] = value;
+    });
+
+  $.ajax({
+    url: url,
+    type: type,
+    data: data,
+    sucess: function(response) {
+
+      if( $('[name="leaveblank"]').val().lenght != 0 ){
+        $('.formphp').html("<div id='form-erro'></div>");
+        $('#form-erro').html("<span>Falha no envio!</span><p>Você pode tentar novamente, ou enviar direto para o e-mail." + emailContato + " </p>")
+        .hide()
+        .fadeIn(1500, function() {
+        $('#form-erro');
+        });
+      } else {
+
+        $('.formphp').html("<div id='form-send'></div>");
+        $('#form-send').html("<span>Mensagem enviada!</span><p>Em breve entraremos em contato com você. Obrigado!</p>")
+        .hide()
+        .fadeIn(1500, function() {
+        $('#form-send');
+        });
+      };
+    },
+    error: function(response) {
+      $('.formphp').html("<div id='form-erro'></div>");
+      $('#form-erro').html("<span>Falha no envio!</span><p>Você pode tentar novamente, ou enviar direto para o e-mail." + emailContato + " </p>")
+        .hide()
+        .fadeIn(1500, function() {
+        $('#form-erro');
+        });
+      }
+    });
+
+    return false;
+
+    });
